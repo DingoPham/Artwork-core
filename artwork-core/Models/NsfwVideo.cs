8301312ba0a1638634d1575d7d6180982ad694dd
DingoPham <pencibambo@gmail.com>
2025-03-01 17:30:51 +0700

    testtsett

--- artwork-core/Models/NsfwVideo.cs
+++ artwork-core/Models/NsfwVideo.cs
@@ -6,7 +6,6 @@
         public string NsfwVideoUrl { get; set; }
         public string NsfwVideoName { get; set; } = "";
         public string NsfwVideoDescribe { get; set; } = "";
-        public string NsfwVideoType { get; set; }
         public int Order { get; set; }
     }
 }
--- artwork-core/Models/Video.cs
+++ artwork-core/Models/Video.cs
@@ -6,7 +6,6 @@
         public string VideoUrl { get; set; }
         public string VideoName { get; set; } = "";
         public string VideoDescribe { get; set; } = "";
-        public string VideoType { get; set; } 
         public int Order { get; set; }
     }
 }
